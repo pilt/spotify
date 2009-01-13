a31590488880df680bf5b2b4b6639379c2c2a3b8
Simon Pantzare <simon@spantz.org>
2009-01-13 01:11:13 +0100

    Add spotify_show function and nps Irssi command to show what np command would send.

--- spotify.pl
+++ spotify.pl
@@ -14,6 +14,8 @@ $VERSION = '1.0';
     changed     => '12 January 2009'
 );
 
+# FIXME: Code duplication in spotify_playing and spotify_show.
+
 sub spotify_playing
 {
 	my($data, $server, $witem, $text, $login, $script) = @_;
@@ -29,6 +31,23 @@ sub spotify_playing
     }
 }
 
+sub spotify_show
+{
+	my($data, $server, $witem, $text, $login, $script) = @_;
+	return unless $witem;
+	$login = Irssi::settings_get_str("login");
+	$script = Irssi::settings_get_str("script_path");
+    $text = `SPOTIFY_LOGIN=$login python $script`;
+    if ($? eq 0) {
+        $text =~ s/\s+$//;
+        Irssi::print("♪ $text $data");
+    }
+    else {
+        Irssi::print("Could not get currently playing song."); 
+    }
+}
+
 Irssi::command_bind np => \&spotify_playing;
+Irssi::command_bind nps => \&spotify_show;
 Irssi::settings_add_str("spotify", "login", "b702003");
 Irssi::settings_add_str("spotify", "script_path", "~/.irssi/scripts/spotify.py");
