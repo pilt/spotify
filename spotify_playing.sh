f604854e2f78679e0c51d58d625345c37527fe85
Simon Pantzare <simon@spantz.org>
2009-05-24 16:29:37 +0200

    use C version if it is available, update README

--- spotify_playing.sh
+++ spotify_playing.sh
@@ -1,11 +1,19 @@
-pgrep spotify.exe 1>/dev/null 2>&1
-if [ $? != 0 ]; then
-    exit 1
-fi
+script_path=`dirname $0`
+c_exec=$script_path/spotify_playing
 
-xwininfo -root -tree | grep '"Spotify": ("spotify.exe" "Wine")' 1>/dev/null 2>&1
-if [ $? != 0 ]; then
-    xwininfo -root -tree | grep '("spotify.exe" "Wine")' | grep -v 'has no name' | sed 's/[ ]*0x[0-9a    -f]* "Spotify - \(.*\)": ("spotify.exe" "Wine").*/\1/'
+if [ -e $c_exec ]; then
+    # Use the C version if it exists.
+    $c_exec
 else
-    exit 1
+    pgrep spotify.exe 1>/dev/null 2>&1
+    if [ $? != 0 ]; then
+        exit 1
+    fi
+
+    xwininfo -root -tree | grep '"Spotify": ("spotify.exe" "Wine")' 1>/dev/null 2>&1
+    if [ $? != 0 ]; then
+        xwininfo -root -tree | grep '("spotify.exe" "Wine")' | grep -v 'has no name' | sed 's/[ ]*0x[0-9a    -f]* "Spotify - \(.*\)": ("spotify.exe" "Wine").*/\1/'
+    else
+        exit 1
+    fi
 fi
