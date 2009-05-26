3308cc1833bce958bd0dfce9e587a158aaccae20
Simon Pantzare <simon@spantz.org>
2009-05-26 23:27:41 +0200

    enhance spotify_util_test.c, make it loop and wait

--- spotify_util_test.c
+++ spotify_util_test.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include "spotify_util.h"
 
-int main(int argc, char **argv)
-{
-    spotify *s;
-    if ((s = spotify_init()) == NULL) {
-        puts("unable to initialize");
-        return -1;
-    }
+spotify *s;
 
+void print_playing(spotify *s)
+{
     if (spotify_update_playing(s))
         printf("artist: %s\ntitle: %s\n", s->playing.artist, s->playing.title);
     else
         puts("unable to update the currently playing song");
+}
+
+int main(int argc, char **argv)
+{
+    if ((s = spotify_init()) == NULL) {
+        puts("unable to initialize\n");
+        return -1;
+    }
+
+    for (;;) {
+        print_playing(s);
+        sleep(1);
+    }
 
     spotify_free(s);
     return 0;
